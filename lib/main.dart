6ac0cd637bd8f69906edd4b363fa302ae51e221d
Byeonggeon Kim <byeonggeon@naver.com>
2023-11-30 16:17:10 -0500

    done

--- lib/main.dart
+++ lib/main.dart
@@ -6,9 +6,14 @@ void main() {
   runApp(const App());
 }
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
   const App({super.key});
 
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
